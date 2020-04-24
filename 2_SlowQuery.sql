27a550c918122c86c51f51614e30cd7b822036d9
Kendra Little <littlekendra@gmail.com>
2020-04-24 17:29:18 +0100

    yolo

---
+++ 2_SlowQuery.sql
@@ -0,0 +1,36 @@
+
+/*👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 
+
+🔨 S E T U P 🔨
+
+Create the pubs database 
+    https://github.com/microsoft/sql-server-samples/blob/master/samples/databases/northwind-pubs/instpubs.sql
+
+This is a Microsoft script shared under the MIT License
+    https://github.com/microsoft/sql-server-samples/blob/master/license.txt
+
+
+
+🏆 C H A L L E N G E 🏆
+
+Write a single query using (any) table or tables in the pubs database. 
+
+The query should last for at least 15 seconds, but not longer than 45 seconds.
+
+You can create objects which your query calls, but please keep them in the pubs database and keep your script as short as possible (not 6000 lines 🙃)
+
+
+SOME RULES:
+* Your query can't change the data in the pubs database, or do anything outside the pubs database
+* No changing system settings (max server memory, etc) 
+* Other queries should still be able to connect to the SQL Server instance and get started, but should have lock waits 
+    (in other words, a causing a BSOD or using all the threads ain't the challenge, LOL)
+
+TIPS:
+* Start simple
+* Get more creative from there
+* Submit your code within 10 minutes, tweet a link to it with #BestWorstCode
+
+👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 👌👎 */
+
+
